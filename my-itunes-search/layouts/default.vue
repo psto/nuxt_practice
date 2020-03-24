28c97b0a0ceaf64bcf8a4030420f64abd5cf8a20
psto <psto@users.noreply.github.com>
2020-03-24 19:51:55 +0100

    initial commit for my-itunes-search

---
+++ my-itunes-search/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container">
+    <div>
+      <ToolBar />
+      <nuxt />
+    </div>
+  </section>
+</template>
+
+<script>
+import ToolBar from '~/components/Toolbar'
+
+export default {
+  components: {
+    ToolBar
+  },
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
